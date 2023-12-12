acf69b1194ca69c2773f1ca4ba4cede2dd858327
JobinMark <113767541+JobinMark@users.noreply.github.com>
2023-12-12 19:36:48 +0530

    Update adtstack.cpp

--- adtstack.cpp
+++ adtstack.cpp
@@ -1,63 +1,79 @@
 #include <iostream>
 
-const int MAX_SIZE = 100;
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
+};
 
 class Stack {
 private:
-    int arr[MAX_SIZE];
-    int top;
+    Node* top;
 
 public:
-    Stack() {
-        top = -1; // Initialize the top of the stack
+    Stack() : top(nullptr) {}
+
+    bool isEmpty() {
+        return top == nullptr;
     }
 
-    void push(int value) {
-        if (top >= MAX_SIZE - 1) {
-            std::cout << "Stack overflow! Cannot push more elements." << std::endl;
-            return;
-        }
-
-        arr[++top] = value;
-        std::cout << value << " pushed to the stack." << std::endl;
+    void push(int data) {
+        Node* newNode = new Node(data);
+        newNode->next = top;
+        top = newNode;
     }
-
     void pop() {
-        if (top < 0) {
-            std::cout << "Stack underflow! Cannot pop from an empty stack." << std::endl;
-            return;
+        if (!isEmpty()) {
+            Node* temp = top;
+            top = top->next;
+            delete temp;
         }
-
-        std::cout << arr[top--] << " popped from the stack." << std::endl;
     }
 
     int peek() {
-        if (top < 0) {
-            std::cout << "Stack is empty. Cannot peek." << std::endl;
-            return -1; // Assuming -1 represents an error or an empty stack in this example
+        if (!isEmpty()) {
+            return top->data;
+        } else {
+            std::cerr << "Stack is empty." << std::endl;
+            return -1; // You can choose a different sentinel value
         }
-
-        return arr[top];
-    }
-
-    bool isEmpty() {
-        return top < 0;
     }
 };
 
 int main() {
-    Stack myStack;
+    Stack stack;
 
-    myStack.push(10);
-    myStack.push(20);
-    myStack.push(30);
+    char choice;
+    int value;
 
-    std::cout << "Top element: " << myStack.peek() << std::endl;
-
-    myStack.pop();
-    myStack.pop();
-
-    std::cout << "Is the stack empty? " << (myStack.isEmpty() ? "Yes" : "No") << std::endl;
-
+    do {
+        std::cout << "1. Push" << std::endl;
+        std::cout << "2. Pop" << std::endl;
+        std::cout << "3. Peek" << std::endl;
+        std::cout << "4. Quit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        switch (choice) {
+            case 1:
+                std::cout << "Enter a value to push: ";
+                std::cin >> value;
+                stack.push(value);
+                break;
+            case 2:
+                stack.pop();
+                std::cout << "Popped one element." << std::endl;
+                break;
+            case 3:
+                std::cout << "Top element: " << stack.peek() << std::endl;
+                break;
+            case 4:
+                std::cout << "Quitting the program." << std::endl;
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+        }
+    } while (choice != 4);
     return 0;
 }
